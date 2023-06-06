7dc9c0d57e71066ea84c4d205fe09b9b6b953685
rufatgit <rufat.valiyev02@gmail.com>
2023-06-06 15:31:25 +0200

    test

--- lab3_pdf_exercises/src/main/java/com/mycompany/lab3_pdf_exercises/Lab3_pdf_exercises.java
+++ lab3_pdf_exercises/src/main/java/com/mycompany/lab3_pdf_exercises/Lab3_pdf_exercises.java
@@ -8,7 +8,32 @@ import java.util.*;
 /**
  *
  * @author rufat
- */
+
+Exercise 1. Create a class Rectangle, which maintains the data about the
+rectangle (height, width) and the methods returning the area and
+perimeter. Write also the appropriate constructors. Test the class by
+creating some objects, anonymous objects and invoke the methods.
+
+Exercise 2. Create a class Pattern, so that it is possible to execute the
+following code:
+Pattern p1 = new Pattern();
+Pattern p2 = new Pattern(’o’,’$’,5);
+System.out.println(p1.giveDescription());
+p1.print();
+System.out.println(p2.giveDescription());
+p2.print();
+p1.changeData(’-’,’+’,7);
+p1.print();
+The result should be as follows:
+Pattern: 1 x (?,?).
+??
+Pattern: 5 x (o,$).
+o$o$o$o$o$
+-+-+-+-+-+-+-+
+* 
+*/
+
+
 public class Lab3_pdf_exercises {
 
     public static class Rect {
